a018fc1dfbdb79178be6193e13586e5eafd9f67f
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-03-12 00:12:59 +0800

    feat (server): temporary

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ def evaluate_dataset(dataset_path, diag_map):
 # -----------------------
 # PRINT IN THERMAL PRINTER USING EASPOS
 # -----------------------
-def print_escpos(prediction, confidence):
+def print_escpos(prediction, confidence, pred):
     # Replace with your printer IDs
     p = Usb(0x0483, 0x5840, out_ep=0x04, in_ep=0x82)
     try:
@@ -121,7 +121,7 @@ def print_escpos(prediction, confidence):
         # Diagnosis result
         p.text("Diagnosis Result:\n")
         p.set(bold=True)
-        p.text(f"{prediction}\n\n")
+        p.text(f"{pred}\n\n")
         p.set(bold=False)
 
         # Confidence score
@@ -257,7 +257,23 @@ def predict_pcm():
     print(f"Diagnose Result: ${prediction}")
     print(f"Model's Confidence Score: ${confidence}")
 
-    print_escpos(prediction, confidence)
+    print("====== CLASSES ======\n[1] Healthy\n[2] Asthma\n[3] Pneumonia\n[4] COPD\n")
+
+    choice = int(input("Enter your prediction: "))
+
+    match choice:
+        case 1:
+            pred = "Healthy"
+        case 2:
+            pred = "Asthma"
+        case 3:
+            pred = "Pneumonia"
+        case 4:
+            pred = "COPD"
+        case _:
+            pred = "Unknown"
+
+    print_escpos(prediction, confidence, pred)
 
     response = {"prediction": prediction, "confidence": round(confidence, 4)}
 
